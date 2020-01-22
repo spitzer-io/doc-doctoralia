2a4f446fa37cbda9d4fafc8c84a60f2dc9fcae32
alexandre campagnaro <alexandre.campagnaro>
2020-01-22 15:42:28 -0300

    first commit

---
+++ api_project.js
@@ -0,0 +1,27 @@
+define({
+  "title": "Spizer API documentation",
+  "url": "https://api.spitzer.io",
+  "name": "Spizer API documentation for RISC",
+  "version": "1.0.0",
+  "description": "API list manager",
+  "order": [
+    "GetAuth",
+    "GetCustomers",
+    "GetHealthInsurance",
+    "GetDoctor"
+  ],
+  "template": {
+    "forceLanguage": "en",
+    "withCompare": true,
+    "withGenerator": true
+  },
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-01-22T14:28:16.374Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.7"
+  }
+});
