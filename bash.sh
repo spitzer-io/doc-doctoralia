a1bfc7a79def63266735aa47634e6e4352edbc3b
Alexandre Campagnaro <alexandre.campagnaro@gmail.com>
2021-01-10 12:04:41 -0300

    Atualizando a doc

---
+++ bash.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Script Para Backup De banco De Dados SQL Server + SQLCMD Para Amazon AWS
+
+# Add arquivos ao git
+echo " "
+echo "-----------------------------------------------------------------------------"
+echo "------------------- $(date) -------------------"
+
+git add -A
+
+# commita os arquivos
+echo " "
+echo "-----------------------------------------------------------------------------"
+echo "------------------- $(date) -------------------"
+
+git commit -m "Atualizando a doc"
+
+# push
+echo " "
+echo "-----------------------------------------------------------------------------"
+echo "------------------- $(date) -------------------"
+
+git push -u origin master
+
+
+echo " Acabou"
+echo "-----------------------------------------------------------------------------"
+echo "------------------- $(date) -------------------"
+
+
+
+npx apidoc -i ./src/http/routes/doctoralia -o ../doc-doctoralia
+npx apidoc -i ./src/http/routes/api-risc -o ../doc-api-risc
